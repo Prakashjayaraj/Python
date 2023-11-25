f6e67fcbbcddc390afad20e8a33173b65fb8d375
Prakashjayaraj <151844782+Prakashjayaraj@users.noreply.github.com>
2023-11-25 18:23:17 +0530

    Assignments

---
+++ Assesment3.py
@@ -0,0 +1,8 @@
+
+# Get input from the user
+number = int(input("Enter a number: "))
+
+print(f"The factors of {number} are:")
+for i in range(1, number + 1):
+    if number % i == 0:
+        print(i)
---
+++ Assignment2.py
@@ -0,0 +1,7 @@
+name =input("enter your name :")
+email_id = input("enter your email id :")
+Phone_number = input("enter your mobile number ")
+
+print("your name is "+ name )
+print("your gmail is "+ email_id)
+print("your mobile number is "+ Phone_number)
