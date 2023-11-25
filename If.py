559796443af743561ed1ce782ce70af296b63f07
Prakashjayaraj <151844782+Prakashjayaraj@users.noreply.github.com>
2023-11-25 18:27:01 +0530

    Add files via upload

---
+++ If.py
@@ -0,0 +1,10 @@
+# If Statemennt
+
+balance = "1000"
+withdraw = "100999"
+if balance < withdraw:
+    print("insufficient balance")
+elif balance == withdraw:
+    print("you have successfully drawn your amount")
+else:
+    print("your withdraw is successfull")
