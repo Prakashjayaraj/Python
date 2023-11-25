4694da47c6aaf8983c63ba5a954408e8228f3820
Prakashjayaraj <151844782+Prakashjayaraj@users.noreply.github.com>
2023-11-25 18:26:32 +0530

    Add files via upload

---
+++ Set.py
@@ -0,0 +1,6 @@
+#set
+employee_id = {"111", "112", "113", "114"}
+employee_name = {"prakash", "venkatesh", "prakash", "pradeep", "Ramki", "Ramki"}
+employee_designation = {"new associate", "associate", "analyst", "specialist"}
+employee_name.add("lokesh")
+print(employee_name)
---
+++ Tuples.py
@@ -0,0 +1,13 @@
+#Tuples:-
+# we can use this but we cannot able to change
+employee_name = ("prakash", "venkatesh", "pradeep", "Santhosh")
+print(employee_name)
+print(employee_name.__contains__("prakash"))
+employee_id = ("13404667", "13404668", "13404669", "12404700")
+print(employee_id)
+employee_designation = ("Associate", "analyst", "new Associate", "Specialist")
+print(employee_designation)
+
+print(employee_id, employee_name, employee_designation)
+
+
